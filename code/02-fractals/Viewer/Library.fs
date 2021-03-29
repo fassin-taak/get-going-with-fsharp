4a4a3431ed0b083fe6a05fa66612a4d15c8f33db
Raghavendra Chandrashekara <raj.chandrashekara@outlook.com>
2021-03-29 17:56:55 +0000

    * Added Preface and continued with additions to Chapter 2. Added FAQ section. * Added ViewerWPF project.

--- code/02-fractals/Viewer/Library.fs
+++ code/02-fractals/Viewer/Library.fs
@@ -1,5 +1,38 @@
-﻿namespace Viewer
+﻿namespace Fractals
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module Fractals =
+  /// Define a type for the fractal set type.
+  type FractalType = Julia | Mandelbrot
+
+  /// The model storing the type.
+  type Model =
+    { Type : FractalType }
+
+  /// Initialization function for the Model type.
+  let Init() = 
+    { Type = Julia }
+
+  /// Messages which can be sent to the fractals Model.
+  type Msg =
+    | SetFractalType of FractalType
+
+  /// Updates the Model based on the message sent and
+  /// returns a new Model.
+  let Update msg m =
+    match msg with
+    | SetFractalType t -> { m with Type = t }
+
+  open Elmish.WPF
+
+  /// Binds the "FractalType" string from XAML views to the
+  /// Model.
+  let View () =
+    [
+      "FractalType" |> Binding.twoWay(
+        (fun m -> m.Type),
+        (fun newValue m -> newValue |> SetFractalType ))
+    ]
+
+  let main window =
+    Program.mkSimpleWpf Init Update View
+    |> Program.runWindow window
